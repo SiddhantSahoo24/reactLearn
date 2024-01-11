abfff0891a5a4543163d5f0f1dafba42b7fb17c0
SiddhantSahoo24 <97823447+SiddhantSahoo24@users.noreply.github.com>
2024-01-12 01:31:55 +0530

    v2

--- App.jsx
+++ App.jsx
@@ -6,52 +6,33 @@ import './App.css'
 function App() {
   
   let [count, setCount] = useState(0)
-  // let addValue = ()=> {
-  //   //console.log("reaching", count)
-  //   //counter=counter+1;
-  //   if(count>20) {
-  //     alert("cant go beyond 20")
-      
-  //   }
-  //   setCount(count+1);
-  // }
-
-  // let decValue = ()=>{
-
-  //    setCount(count-1);
-  // }
+  
 
   return (
     <div>
       
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <h1>{count}</h1>
-        <button onClick={()=> {
-    //console.log("reaching", count)
-    //counter=counter+1;
-    if(count>20) {
-      alert("cant go beyond 20")
-      
-    }
-    setCount(count+1);
-  }
-}>Add Value {count}</button>
-        <br></br>
-        <br></br>
-        <button onClick={()=> setCount(++count)}>Decrease Value</button>
-        <h2>{count}</h2>
-        <br></br>
-        <h4>{count}</h4>
-        <br></br>
-        <p>{count}</p>
+        <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
 }
 
-function customButton(){
-  
+function CustomButton(props){
+
+  return(
+    <>
+    <h1>{props.count}</h1>
+    <br></br>
+    <h2>{props.count}</h2>
+    <br></br>
+    <button onClick={()=>{
+      props.setCount(props.count+1)
+    }}>Count is {props.count}</button>
+    </>
+  )
+
 }
+
+  
+
 
 export default App
